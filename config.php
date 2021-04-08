a5b058fe1e91e9ecc1898d4d92143487b30bd59a
root@DZGIF <38464836+dzgif@users.noreply.github.com>
2021-04-08 10:36:57 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+// list
+$inputList = 'list.txt'; // list.txt
+
+// telegram
+$telegramToken = '1764889246:AAH1C1HEKE5cd9EedPCdcreuTacFgzZsyoQ'; // token telegram bot
+$telegramChatId = '1094646089'; // chat id telegram
